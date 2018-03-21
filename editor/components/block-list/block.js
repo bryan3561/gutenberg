6dcfc15571f339e88c50affbd591dc47561b4189
Andrew Duthie <andrew@andrewduthie.com>
2018-03-21 09:05:53 -0400

    Block List: Update insertion point comment per last->first

--- editor/components/block-list/block.js
+++ editor/components/block-list/block.js
@@ -439,7 +439,7 @@ export class BlockListBlock extends Component {
 
 		// Insertion point can only be made visible when the side inserter is
 		// not present, and either the block is at the extent of a selection or
-		// is the last block in the top-level list rendering.
+		// is the first block in the top-level list rendering.
 		const shouldShowInsertionPoint = (
 			( ! isMultiSelected && ! isFirst ) ||
 			( isMultiSelected && isLastInSelection ) ||
